1990627e12a2cd604951eb2ca150fa7c05430f26
Willian Hercilio Pezzini <williantijucas@gmail.com>
2024-11-18 06:51:31 -0300

    Edita e adicina funções no Listar

--- resources/views/app/fornecedor/listar.blade.php
+++ resources/views/app/fornecedor/listar.blade.php
@@ -7,7 +7,7 @@
         <div class='titulo-pagina-2'>
             <p>Listar - Fornecedor</p>
         </div>
-    <br>
+        <br>
         <div class='menu'>
             <ul>
                 <li><a href='{{ route('app.fornecedor.adicionar') }}'>Novo</a></li>
@@ -15,9 +15,9 @@
                 <li><a href='{{ route('app.fornecedor') }}'>Voltar</a></li>
             </ul>
         </div>
-    <br>
-    <br>
-    <br>
+        <br>
+        <br>
+        <br>
         <div class='informação-pagina'>
             <div style='width: 80%; padding: 20px 0px; margin-left: auto; margin-right: auto;'>
                 <table border="1" width='100%'>
@@ -27,29 +27,57 @@
                             <th>Site</th>
                             <th>UF</th>
                             <th>E-mail</th>
-                            <th></th>
-                            <th></th>
                         </tr>
                     </thead>
-
                     <tbody>
                         @foreach ($fornecedores as $fornecedor)
                             <tr>
-                                <td>{{ $fornecedor->nome}}</td>
-                                <td>{{ $fornecedor->site}}</td>
-                                <td>{{ $fornecedor->uf}}</td>
-                                <td>{{ $fornecedor->email}}</td>
+                                <td>{{ $fornecedor->nome }}</td>
+                                <td>{{ $fornecedor->site }}</td>
+                                <td>{{ $fornecedor->uf }}</td>
+                                <td>{{ $fornecedor->email }}</td>
                                 <td><a href="{{ route('app.fornecedor.excluir', $fornecedor->id) }}"> Excluir</a></td>
                                 <td><a href="{{ route('app.fornecedor.editar', $fornecedor->id) }}"> Editar</a></td>
                             </tr>
+
+                            {{-- Exibe os produtos relacionados junto com a lista de Fornecedores --}}
+                            {{-- <tr>
+                                <td colspan="6">
+                                    <p>Lista de produtos</p>
+                                    <table border="1" style="margin:20px">
+
+                                        <thead>
+                                            <tr>
+                                                <th>ID</th>
+                                                <th>Nome</th>
+                                            </tr>
+                                        </thead>
+
+                                        <tbody>
+                                            @foreach ($fornecedor->produtos as $produto)
+                                                {
+                                                    <tr>
+                                                        <td>{{ $produto->id }}
+                                                        <td>{{ $produto->nome }}
+                                                    </tr>
+                                                }
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr> --}}
                         @endforeach
                     </tbody>
                 </table>
 
-                {{ $fornecedores->appends($request)->links()}}                
+
+
+
+
+                {{ $fornecedores->appends($request)->links() }}
             </div>
         </div>
 
     </div>
-   
+
 @endsection
